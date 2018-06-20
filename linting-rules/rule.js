8965b4146b0185b7b09a52a5ff16b58c11d52667
Anton Rybochkin <anton.rybochkin@axibase.com>
2018-06-20 23:05:29 +0300

    rule fix

--- linting-rules/rule.js
+++ linting-rules/rule.js
@@ -5,6 +5,37 @@ class Rule {
         this.regex = new RegExp("\\b" + pattern + "\\b", modifiers);
         this.suggestion = suggestion;
     }
+
+    test(line) {
+        return new Match(line.match(this.regex));
+    }
+}
+
+class Match {
+    constructor(match) {
+        this.match = match;
+    }
+
+    found() {
+        return !!this.match;
+    }
+
+    range() {
+        if (this.match) {
+            let column = this.match.index + 1;
+            let length = this.match[0].length;
+            if (match[2]) {
+                column += this.match[1].length;
+                length -= this.match[1].length;
+            }
+            return [ column, length ];
+        }
+        return null;
+    };
+
+    toString() {
+        return this.match ? "null" : this.match.toString();
+    }
 }
 
 module.exports = Rule;
