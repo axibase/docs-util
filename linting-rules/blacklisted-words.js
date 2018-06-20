3fa7490b5da0c7536296f84f5e07eb1b2ca9c88c
Anton Rybochkin <anton.rybochkin@axibase.com>
2018-06-20 23:33:54 +0300

    move classes back

--- linting-rules/blacklisted-words.js
+++ linting-rules/blacklisted-words.js
@@ -19,7 +19,45 @@
  * Plugin locates patterns prohibited in Axibase style guide.
  */
 
-const Rule = require("./rule");
+class Rule {
+    constructor(pattern, suggestion, caseSensitive) {
+        this.pattern = pattern;
+        const modifiers = caseSensitive ? "" : "i";
+        this.regex = new RegExp("\\b" + pattern + "\\b", modifiers);
+        this.suggestion = suggestion;
+    }
+
+    test(line) {
+        return new Match(line.match(this.regex));
+    }
+}
+
+class Match {
+    constructor(match) {
+        this.match = match;
+    }
+
+    found() {
+        return !!this.match;
+    }
+
+    range() {
+        if (this.match) {
+            let column = this.match.index + 1;
+            let length = this.match[0].length;
+            if (this.match[2]) {
+                column += this.match[1].length;
+                length -= this.match[1].length;
+            }
+            return [ column, length ];
+        }
+        return null;
+    }
+
+    toString() {
+        return this.match ? this.match.toString() : "null";
+    }
+}
 
 const rules = [
     new Rule("should", "use 'must' or remove"),
