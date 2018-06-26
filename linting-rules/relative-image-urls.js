c6f3c0f81c484fab28b0c3112759e06138fa6410
unrealwork <unrealwork@gmail.com>
2018-06-26 10:02:18 +0300

    tokens check

--- linting-rules/relative-image-urls.js
+++ linting-rules/relative-image-urls.js
@@ -28,25 +28,28 @@ module.exports = {
     description: "Relative URLs to images must start with ./ or ../",
     tags: ["links"],
     "function": (params, onError) => {
-        params.tokens.filter(t => t.type === "inline").forEach(token => {
-            let images = token.children.filter(t => t.type === "image")
-            for (let img of images) {
-                let src = img.attrGet("src")
-                if (src) {
-                    let isExternal = testExternal.test(src);
-                    let isValidRelative = testValidRelative.test(src);
-                    if (!isExternal && !isValidRelative) {
-                        let index = img.line.indexOf(src);
-                        let range = [index + 1, src.length];
-                        onError({
-                            lineNumber: img.lineNumber,
-                            details: `In the image for ${img.content}`,
-                            context: `![${src}](${img.content})`,
-                            range,
-                        })
+        let tokens = params.tokens;
+        if (tokens) {
+            tokens.filter(t => t.type === "inline").forEach(token => {
+                let images = token.children.filter(t => t.type === "image")
+                for (let img of images) {
+                    let src = img.attrGet("src")
+                    if (src) {
+                        let isExternal = testExternal.test(src);
+                        let isValidRelative = testValidRelative.test(src);
+                        if (!isExternal && !isValidRelative) {
+                            let index = img.line.indexOf(src);
+                            let range = [index + 1, src.length];
+                            onError({
+                                lineNumber: img.lineNumber,
+                                details: `In the image for ${img.content}`,
+                                context: `![${src}](${img.content})`,
+                                range,
+                            })
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
     }
 };
