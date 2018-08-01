37f516f24fd13d977f03557c8073400d9af03530
Anton Rybochkin <anton.rybochkin@axibase.com>
2018-08-01 16:16:00 +0300

    better arguments parsing. modified generated pattern

--- python-scripts/dictionaries_generator.py
+++ python-scripts/dictionaries_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import sys
 import urllib.request as req
@@ -19,7 +20,7 @@ $1 mode. Can be "atsd", "legacy" or "default".
            the old dictionary file .spelling
     "default" downloads base dictionaries and merges their content with current repository
     dictionaries
-$2 names dictionary filename (default is ".dictionary-name")
+$2 names dictionary filename (default is ".dictionary-names")
 $3 other dictionary filename (default is ".dictionary-other")
 """
 
@@ -31,7 +32,13 @@ DEFAULT_JSON_DICTIONARY_DESTINATION = ".yaspeller-dictionary.json"
 
 
 def wrap_pattern(pattern: str) -> str:
-    return ".*?\\b{}\\b.*?".format(pattern)
+    """
+    wraps the pattern to word boundaries.
+    punctuation marks are checked to reduce number of false-positive matches in spellchecker-cli
+    :param pattern: original regex
+    :return: modified regex pattern
+    """
+    return "\\b{}\\b[.,:?!)]*?".format(pattern)
 
 
 def modify_regex(pattern: str) -> str:
@@ -59,24 +66,39 @@ def download_file(filename) -> IO:
     return req.urlopen(BASE_DICTIONARY_LOCATION + filename)
 
 
-if __name__ == '__main__':
-    mode = sys.argv[1] if len(sys.argv) > 1 else "default"
-    names_dictionary_name = sys.argv[2] if len(sys.argv) > 1 else DEFAULT_NAMES_DICTIONARY_FILENAME
-    words_dictionary_name = sys.argv[3] if len(sys.argv) > 2 else DEFAULT_OTHER_DICTIONARY_FILENAME
-    if DEFAULT_PLAIN_DICTIONARY_DESTINATION in (names_dictionary_name, words_dictionary_name):
+def validate_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Spellchecking dictionaries converter.')
+    parser.add_argument('--mode', '-m', type=str,
+                        help="mode: atsd (don't download additional dictionaries), "
+                             "legacy (download .spelling file), "
+                             "default (download .dictionary-names, .dictionary-other from atsd repository)",
+                        default="default")
+    parser.add_argument('--names-dictionary', '-nd', type=str,
+                        help='Names dictionary filename, first letter in each word in file stays unchanged',
+                        default=DEFAULT_NAMES_DICTIONARY_FILENAME)
+    parser.add_argument('--words-dictionary', '-d', type=str,
+                        help='Other dictionary filename',
+                        default=DEFAULT_OTHER_DICTIONARY_FILENAME)
+    args = parser.parse_args()
+
+    if DEFAULT_PLAIN_DICTIONARY_DESTINATION in (args.names_dictionary, args.words_dictionary):
         sys.stderr("Source dictionary filename must not be equal to {}\n".format(DEFAULT_PLAIN_DICTIONARY_DESTINATION))
         exit(1)
+    return args
 
-    dictionary_words = set()
-    if mode.lower() == "legacy":
-        convert_dictionary(lambda: download_file(DEFAULT_PLAIN_DICTIONARY_DESTINATION), wrap_pattern, dictionary_words)
-    elif mode.lower() != "atsd":
-        convert_dictionary(lambda: download_file(DEFAULT_NAMES_DICTIONARY_FILENAME), wrap_pattern, dictionary_words)
-        convert_dictionary(lambda: download_file(DEFAULT_OTHER_DICTIONARY_FILENAME), modify_regex, dictionary_words)
 
-    convert_dictionary(lambda: open(names_dictionary_name), wrap_pattern, dictionary_words)
-    convert_dictionary(lambda: open(words_dictionary_name), modify_regex, dictionary_words)
-    sorted_dictionary = list(sorted(dictionary_words))
+if __name__ == '__main__':
+    args = validate_arguments()
+    patterns = set()
+    if args.mode.lower() == "legacy":
+        convert_dictionary(lambda: download_file(DEFAULT_PLAIN_DICTIONARY_DESTINATION), wrap_pattern, patterns)
+    elif args.mode.lower() != "atsd":
+        convert_dictionary(lambda: download_file(DEFAULT_NAMES_DICTIONARY_FILENAME), wrap_pattern, patterns)
+        convert_dictionary(lambda: download_file(DEFAULT_OTHER_DICTIONARY_FILENAME), modify_regex, patterns)
+
+    convert_dictionary(lambda: open(args.names_dictionary), wrap_pattern, patterns)
+    convert_dictionary(lambda: open(args.words_dictionary), modify_regex, patterns)
+    sorted_dictionary = list(sorted(patterns))
 
     with open(DEFAULT_PLAIN_DICTIONARY_DESTINATION, 'w') as out:
         out.write('\n'.join(sorted_dictionary))
