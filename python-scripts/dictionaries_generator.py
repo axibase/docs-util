12249b2f57d88f5e8b45d2f3a3268efb2bc99349
Anton Rybochkin <anton.rybochkin@axibase.com>
2018-08-01 16:49:16 +0300

    scrtipt update

--- python-scripts/dictionaries_generator.py
+++ python-scripts/dictionaries_generator.py
@@ -13,20 +13,20 @@ spellchecker-cli and yaspeller tools. As a result it creates two files in a work
 - .yaspeller-dictionary.json (format recognized by yaspeller)
 
 Optional arguments:
-$1 mode. Can be "atsd", "legacy" or "default".
+--mode. Can be "atsd", "legacy" or "default".
     "atsd" means that the script is running in atsd repository and it
            doesn't need to download base dictionary
-    "legacy" means that the base repository (atsd) dictionary is not updated, so download
-           the old dictionary file .spelling
-    "default" downloads base dictionaries and merges their content with current repository
+    "legacy" means that current repo is not updated to new format, so a single .dictionary file should be used
+    "default" downloads base dictionaries and merges their content with current repository dictionaries
     dictionaries
-$2 names dictionary filename (default is ".dictionary-names")
-$3 other dictionary filename (default is ".dictionary-other")
+--names-dictionary filename of dictionary with case-sensitive patterns (default is ".dictionary-names")
+--words-dictionary filename of dictionary with patterns with case-insensitive capital (default is ".dictionary-other")
 """
 
 BASE_DICTIONARY_LOCATION = "https://raw.githubusercontent.com/axibase/atsd/master/"
 DEFAULT_NAMES_DICTIONARY_FILENAME = ".dictionary-names"
 DEFAULT_OTHER_DICTIONARY_FILENAME = ".dictionary-other"
+DEFAULT_LEGACY_DICTIONARY_FILENAME = ".dictionary"
 DEFAULT_PLAIN_DICTIONARY_DESTINATION = ".spelling"
 DEFAULT_JSON_DICTIONARY_DESTINATION = ".yaspeller-dictionary.json"
 
@@ -41,7 +41,7 @@ def wrap_pattern(pattern: str) -> str:
     return "\\b{}\\b[.,:?!)]*?".format(pattern)
 
 
-def modify_regex(pattern: str) -> str:
+def to_case_insensitive_regex(pattern: str) -> str:
     first_letter = pattern[0]
     if first_letter.isalpha():
         return wrap_pattern("[{}{}]{}".format(first_letter.upper(), first_letter.lower(), pattern[1:]))
@@ -70,8 +70,9 @@ def validate_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Spellchecking dictionaries converter.')
     parser.add_argument('--mode', '-m', type=str,
                         help="mode: atsd (don't download additional dictionaries), "
-                             "legacy (download .spelling file), "
-                             "default (download .dictionary-names, .dictionary-other from atsd repository)",
+                             "legacy (load single .dictionary file from current repo), "
+                             "default (download .dictionary-names, .dictionary-other from atsd repository, "
+                             "merge with similar files in current repo)",
                         default="default")
     parser.add_argument('--names-dictionary', '-nd', type=str,
                         help='Names dictionary filename, first letter in each word in file stays unchanged',
@@ -90,14 +91,14 @@ def validate_arguments() -> argparse.Namespace:
 if __name__ == '__main__':
     args = validate_arguments()
     patterns = set()
-    if args.mode.lower() == "legacy":
-        convert_dictionary(lambda: download_file(DEFAULT_PLAIN_DICTIONARY_DESTINATION), wrap_pattern, patterns)
-    elif args.mode.lower() != "atsd":
+    if args.mode.lower() != "atsd":
         convert_dictionary(lambda: download_file(DEFAULT_NAMES_DICTIONARY_FILENAME), wrap_pattern, patterns)
-        convert_dictionary(lambda: download_file(DEFAULT_OTHER_DICTIONARY_FILENAME), modify_regex, patterns)
-
-    convert_dictionary(lambda: open(args.names_dictionary), wrap_pattern, patterns)
-    convert_dictionary(lambda: open(args.words_dictionary), modify_regex, patterns)
+        convert_dictionary(lambda: download_file(DEFAULT_OTHER_DICTIONARY_FILENAME), to_case_insensitive_regex, patterns)
+    if args.mode.lower() == "legacy":
+        convert_dictionary(lambda: open(DEFAULT_LEGACY_DICTIONARY_FILENAME), to_case_insensitive_regex, patterns)
+    else:
+        convert_dictionary(lambda: open(args.names_dictionary), wrap_pattern, patterns)
+        convert_dictionary(lambda: open(args.words_dictionary), to_case_insensitive_regex, patterns)
     sorted_dictionary = list(sorted(patterns))
 
     with open(DEFAULT_PLAIN_DICTIONARY_DESTINATION, 'w') as out:
